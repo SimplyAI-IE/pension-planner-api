431c8fccdb059459ecae7b90de4cb497ba7708ed
Jason Cooke <admin@simplyai.ie>
2025-04-15 13:24:26 +0100

    Switch to gpt-3.5-turbo

--- gpt_engine.py
+++ gpt_engine.py
@@ -41,7 +41,7 @@ def get_gpt_response(user_input, user_id):
     ]
 
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=messages
     )
 
